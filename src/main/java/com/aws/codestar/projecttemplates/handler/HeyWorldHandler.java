5e180e52816145bdc42c14c7452abbd3c0959e1c
uopsdod <uopsdod@gmail.com>
2018-11-13 02:14:01 +0800

    change to two lambdas with GET

---
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HeyWorldHandler.java
@@ -0,0 +1,22 @@
+package com.aws.codestar.projecttemplates.handler;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.aws.codestar.projecttemplates.GatewayResponse;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class HeyWorldHandler implements RequestHandler<Object, Object> {
+
+    public Object handleRequest(final Object input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return new GatewayResponse(new JSONObject().put("Output", "Hey World 777 !").toString(), headers, 200);
+    }
+}
